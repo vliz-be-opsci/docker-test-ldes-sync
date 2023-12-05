d2058574463e9dbb3729e2bd7f132fb5174bf975
Marc Portier <marc.portier@gmail.com>
2023-12-05 14:18:57 +0100

    allow to inject LDES_DEBUG_LEVEL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # this script sends operational storage to stderr, and the actual ldes output to stdout
+LL="${LDES_LOGGING_LEVEL:-error}"
 >&2 echo "using node $(node --version)"
->&2 echo "checking feed '$1'" 
-actor-init-ldes-client --disableSynchronization true --dereferenceMembers true --requestsPerMinute 60 --mimeType text/turtle --loggingLevel error $1 
+>&2 echo "checking feed '$1' at logging Level  '${LL}'" 
+actor-init-ldes-client --disableSynchronization true --dereferenceMembers true --requestsPerMinute 60 --mimeType text/turtle --loggingLevel ${LL} $1 
 >&2 echo "execution ended"
